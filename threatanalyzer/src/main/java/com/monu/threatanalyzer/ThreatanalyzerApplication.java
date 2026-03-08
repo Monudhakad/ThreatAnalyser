9dfda0ae10fc3b93664b5aab853d0891e330dbd9
Monudhakad <monudhakad2332005@gmail.com>
2026-03-08 17:16:25 +0530

    Initial Spring Boot project setup with test API

---
+++ threatanalyzer/src/main/java/com/monu/threatanalyzer/ThreatanalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.monu.threatanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreatanalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThreatanalyzerApplication.class, args);
+	}
+
+}
