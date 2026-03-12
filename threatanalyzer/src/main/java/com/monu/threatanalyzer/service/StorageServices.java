9af854071da2559e712992370abaefb09fb69eed
Monudhakad <monudhakad2332005@gmail.com>
2026-03-12 16:22:17 +0530

    feat (storage): implemented zip persistence system and scanid based file naming

--- threatanalyzer/src/main/java/com/monu/threatanalyzer/service/StorageServices.java
+++ threatanalyzer/src/main/java/com/monu/threatanalyzer/service/StorageServices.java
@@ -3,14 +3,16 @@ package com.monu.threatanalyzer.service;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.Files;
- import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Service;
 import jakarta.annotation.PostConstruct;
 
-@Service
+@Service 
+
 public class StorageServices {
     private final Path uploadPath = Paths.get("workspace/uploads");
 
     @PostConstruct
+    //to create a directory to save the file.
     public void initStorage(){
 
         try{
@@ -22,4 +24,23 @@ public class StorageServices {
             throw new RuntimeException("could not create upload folder");
         }
     }
+
+    // to accept file and scan id for file and then return the file path
+    public String saveZip(byte[] data, String scanId){
+        try{
+            Path filepath = uploadPath.resolve(scanId + ".zip");
+            Files.write(filepath, data);
+            System.out.println("ZIP saved at: "+ filepath.toAbsolutePath());
+            return filepath.toString();
+        } catch(Exception e){
+            throw new RuntimeException("Failed to save the zip file");
+        }
+    }
+    // to generate Scan id which will be sticked with the filename.
+    public String generateScanId(){
+        return java.util.UUID.randomUUID()
+                .toString()
+                .substring(0, 8);
+    }
+    
 }
