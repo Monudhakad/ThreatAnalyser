a8fb5dd46e0e6bc0e66323860c4ec6bb61e34861
Monudhakad <monudhakad2332005@gmail.com>
2026-03-21 16:57:40 +0530

    feat: implemented dependency check (basic)

--- threatanalyzer/src/main/java/com/monu/threatanalyzer/service/ScanService.java
+++ threatanalyzer/src/main/java/com/monu/threatanalyzer/service/ScanService.java
@@ -14,6 +14,9 @@ public class ScanService {
             Path projectRoot = uploadRoot.resolve(scanId).resolve("extracted");
             System.out.println("scanning directory: "+ projectRoot);
             Files.walk(projectRoot).filter(Files::isRegularFile).forEach(file ->{
+                if(file.getFileName().toString().equalsIgnoreCase("pom.xml")){
+                    checkpomDependencies(file);
+                }
                 try {
                     String content = Files.readString(file);
                     if(content.toLowerCase().contains("password")|| content.toLowerCase().contains("secret")|| content.toLowerCase().contains("api_key")|| content.toLowerCase().contains("token")){
@@ -26,6 +29,23 @@ public class ScanService {
         });
         } catch (Exception e) {
             throw new RuntimeException("Scan failed", e);
+        }
+    }
+    private void checkpomDependencies(Path pomPath){
+        try {
+            String content = Files.readString(pomPath);
+            System.out.println("Analyzing dependencies in: "+ pomPath);
+
+            checkDependency(content, "log4j", "2.14");
+            checkDependency(content, "spring-core", "5.2");
+            checkDependency(content, "jackson-databind", "2.9");
+        } catch (Exception e) {
+            System.out.println("Failed to analyze now");
+        }
+    }
+    private void checkDependency(String content, String lib, String vulnVersion){
+        if (content.contains(lib)&& content.contains(vulnVersion)) {
+            System.out.println("Vulnerable dependency detected: "+ lib + " " + vulnVersion);
         }
     }
 }
